ea4b2e8a8d43f52abf35ac374e697e977c705a71
Neha Ajith G <120302847+Neha-Ajith-G@users.noreply.github.com>
2024-07-11 18:18:02 +0000

    It works now

--- ComplexMain.java
+++ ComplexMain.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 
 class Complex{
   public int real,img;
+  public Complex result;
+
+  public Complex(){
+    this.real = 0;
+    this.img = 0;
+  }
   public Complex( int real, int img){
     this.real = real;
     this.img = img;
@@ -12,28 +18,19 @@ class Complex{
     System.out.println( real + " + "+ img + "i");
   }
 
-  static class ComplexMethods{
-    Complex c1;
-    Complex c2; 
-    private int r, i;
 
-    public ComplexMethods(Complex c1,Complex c2){
-      this.c1 = c1;
-      this.c2 = c2;
-    }
-
-    public void addNos(){
-      int r = c1.real + c2.real;
-      int i = c1.img + c2.img;
-      Complex result = new Complex(r,i);
-      result.displayNo();
-    };
+  public void addNos(Complex c1, Complex c2){
+    int r = c1.real + c2.real;
+    int i = c1.img + c2.img;
+    Complex result = new Complex(r,i);
+    result.displayNo();
   }
+  
 }
 
 
-class ComplexMain {
-  public int r1,i1,r2,i2;
+class ComplexMain extends Complex{
+  public int r1, i1, r2, i2;
   public static void main(String args[]){
 
     Scanner scanner = new Scanner(System.in);
@@ -50,17 +47,18 @@ class ComplexMain {
     System.out.println("Input img value for second no:");
     int i2 = scanner.nextInt();
 
-    Complex complex1 = new  Complex(r1,i1);
-    Complex complex2 = new Complex(r2,i2);
+    Complex complex1 = new  Complex(r1, i1);
+    Complex complex2 = new Complex(r2, i2);
 
 
     complex1.displayNo();
     complex2.displayNo();
 
-    Complex.ComplexMethods ans = new Complex.ComplexMethods(complex1,complex2);
+    Complex ans = new Complex(0,0);
+
 
     System.out.println("SUM : ");
-    ans.addNos();
+    ans.addNos(complex1, complex1);
 
     scanner.close();
 
